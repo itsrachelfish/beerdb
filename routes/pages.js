56d00c94aab65eedce1e418fb99d8faac6787100
Rachel Fish <itsrachelfish@gmail.com>
2015-03-31 02:56:10 -0600

    Converted page routes to use an event emitter

--- routes/pages.js
+++ routes/pages.js
@@ -1,58 +1,43 @@
+var extend = require('util')._extend;
+var events = require('events')
+var event = new events.EventEmitter();
+
+event.on('render', function(req, res, options)
+{
+    var defaults =
+    {
+        session: req.session,
+        partials:
+        {
+            head: 'partials/head',
+            sidebar: 'partials/sidebar',
+            foot: 'partials/foot'
+        }
+    };
+
+    options = extend(defaults, options);
+    res.render(options.view, options);
+});
+
 module.exports = function(app)
 {
     app.get('/', function(req, res)
     {
-        res.render('index',
-        {
-            session: req.session,
-            partials:
-            {
-                head: 'partials/head',
-                sidebar: 'partials/sidebar',
-                foot: 'partials/foot'
-            }
-        });
+        event.emit('render', req, res, {view: 'index'});        
     });
 
     app.get('/view', function(req, res)
     {
-        res.render('view',
-        {
-            session: req.session,
-            partials:
-            {
-                head: 'partials/head',
-                sidebar: 'partials/sidebar',
-                foot: 'partials/foot'
-            }
-        });
+        event.emit('render', req, res, {view: 'view'});
     });
 
     app.get('/create', function(req, res)
     {
-        res.render('create',
-        {
-            session: req.session,
-            partials:
-            {
-                head: 'partials/head',
-                sidebar: 'partials/sidebar',
-                foot: 'partials/foot'
-            }
-        });
+        event.emit('render', req, res, {view: 'create'});
     });
 
     app.get('/search', function(req, res)
     {
-        res.render('search',
-        {
-            session: req.session,
-            partials:
-            {
-                head: 'partials/head',
-                sidebar: 'partials/sidebar',
-                foot: 'partials/foot'
-            }
-        });
+        event.emit('render', req, res, {view: 'search'});
     });
 }
