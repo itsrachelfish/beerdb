4cf9dc737db01175775aba057da397cd0d90460a
Rachel Fish <itsrachelfish@gmail.com>
2015-03-31 03:29:38 -0600

    Moved event emitters into server.js, fixed login page

--- routes/login.js
+++ routes/login.js
@@ -1,20 +1,28 @@
-module.exports = function(app)
+var login = require("../login/sdk/server/wetfish-login");
+var config = require("../config.js");
+
+login.init(config.login);
+
+module.exports = function(app, event)
 {
     app.get('/login', function(req, res)
     {
+        if(!req.query.token)
+        {
+            event.emit('message', req, res, {type: 'error', label: ' ', text: 'nope'});
+            return;
+        }
+        
         login.verify(req.query.token, function(verified)
         {
-            if(verified.status == "success")
+            if(verified.status != "success")
             {
-                req.session.user = verified.data;
-                res.send("You're logged in!");
-            }
-            else
-            {
-                res.send("There was an error!<p><b>" + verified.message + "</b></p>");
+                event.emit('message', req, res, {type: 'error', text: verified.message});
+                return;
             }
 
-            res.end();
+            req.session.user = verified.data;
+            event.emit('message', req, res, {type: 'success', text: "You're logged in!"});
         });
     });
 }
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var redis = require('redis');
 var mysql = require('mysql');
 var express = require('express');
+
 var app = express();
 var server = require('http').createServer(app);
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 var bodyParser = require('body-parser');
 
-var login = require("./login/sdk/server/wetfish-login");
-var config = require("./config.js");
+var extend = require('util')._extend;
+var events = require('events')
+var event = new events.EventEmitter();
 
-login.init(config.login);
+var config = require("./config.js");
 
 server.listen(3000);
 console.log("BeerDB server started");
@@ -34,6 +36,54 @@ app.use(express.static(__dirname + '/static'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hjs');
 
-require('./routes/login')(app);
-require('./routes/pages')(app);
-require('./routes/rest')(app);
+// Helper events
+event.on('render', function(req, res, options)
+{
+    var defaults =
+    {
+        session: req.session,
+        partials:
+        {
+            head: 'partials/head',
+            sidebar: 'partials/sidebar',
+            foot: 'partials/foot'
+        }
+    };
+
+    options = extend(defaults, options);
+    res.render(options.view, options);
+});
+
+// Special handler for messages
+event.on('message', function(req, res, message)
+{
+    message = (message || {type: ''});
+    message.type = message.type.toLowerCase();
+    
+    if(message.type == 'success')
+    {
+        message.class = 'success';
+        message.label = (message.label || 'Success!');
+    }
+    else if(message.type == 'error')
+    {
+        message.class = 'danger';
+        message.label = (message.label || 'Error:');
+    }
+    else
+    {
+        message.class = 'info';
+    }
+
+    var options =
+    {
+        view: 'message',
+        message: message
+    }
+    
+    event.emit('render', req, res, options);
+});
+
+require('./routes/login')(app, event);
+require('./routes/pages')(app, event);
+require('./routes/rest')(app, event);
