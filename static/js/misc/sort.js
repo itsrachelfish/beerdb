21e96ff5e061eb7c27f5c87550dec7cb8eb54f84
Rachel Fish <itsrachelfish@gmail.com>
2015-04-21 23:42:37 -0600

    Added query parser to update the url on sort

--- static/js/misc/sort.js
+++ static/js/misc/sort.js
@@ -1,11 +1,24 @@
+// Query string parser, from npm
+var query = require('query-string');
+
 $(document).ready(function()
 {
+    var parsed = query.parse(location.search);
+    
     $('body').on('click', '.sort-asc', function()
     {
         var column = $(this).parents('th').data('column');
         var type = $(this).parents('th').data('type');
-        
-        console.log(column, type, "asc!");
+
+        if(type == 'Range')
+        {
+            column = column + "_min";
+        }
+
+        parsed.column = column;
+        parsed.sort = 'asc';
+
+        location = location.origin + location.pathname + '?' + query.stringify(parsed);
     });
 
     $('body').on('click', '.sort-desc', function()
@@ -13,6 +26,14 @@ $(document).ready(function()
         var column = $(this).parents('th').data('column');
         var type = $(this).parents('th').data('type');
 
-        console.log(column, type, "desc!");
+        if(type == 'Range')
+        {
+            column = column + "_max";
+        }
+
+        parsed.column = column;
+        parsed.sort = 'desc';
+
+        location = location.origin + location.pathname + '?' + query.stringify(parsed);
     });
 });
